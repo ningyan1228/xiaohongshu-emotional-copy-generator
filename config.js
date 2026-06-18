44cb1a76d29d695d593c34bee5466ed0b4a59301
止鹜 <199219863+ningyan1228@users.noreply.github.com>
2026-06-19 07:43:52 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-window.XHS_API_BASE_URL = "https://xiaohongshu-emotional-copy-g-n7p1hd35vd0x.gqzning.deno.net";
+window.XHS_API_BASE_URL = "https://xiaohongshu-emotional-copy-g-yn5ethc2877e.gqzning.deno.net";
